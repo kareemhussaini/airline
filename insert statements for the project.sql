bae63b845450919a8cda1c07c41e2e5041ae749d
kareemhussaini <36721989+kareemhussaini@users.noreply.github.com>
2022-11-21 21:07:29 -0600

    Add files via upload

---
+++ insert statements for the project.sql
@@ -0,0 +1,75 @@
+
+insert into city(title,state) values ('Chicago', 'IL');
+insert into city(title,state) values ('Denver', 'CO');
+insert into city(title,state) values ('Atlanta', 'GA');
+insert into city(title,state) values ('Houston', 'TX');
+insert into city(title,state) values ('Las Vegas', 'NV');
+insert into city(title,state) values ('New York', 'NY');
+insert into city(title,state) values ('pittsburgh', 'PA');
+insert into city(title,state) values ('Seattle', 'WA');
+insert into city(title,state) values ('San Diego', 'CA');
+insert into city(title,state) values ('Portland', 'OR');
+
+--------------------------------------------------------------------
+
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (158,'2022-12-3','20:00',285,1,31,16,7,3);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (172,'2022-12-20','1:00',228,1,33,20,4,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (129,'2022-12-10','4:00',363,1,39,13,2,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (130,'2022-12-7','20:00',479,1,99,20,2,5);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (131,'2022-12-11','20:00',420,1,92,12,2,7);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (145,'2022-12-15','16:00',520,1,57,3,10,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (152,'2022-12-20','19:00',350,1,75,1,9,4);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (175,'2022-12-12','21:00',495,1,58,19,8,5);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (112,'2022-12-23','13:00',381,1,74,14,7,2);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (148,'2022-12-11','14:00',484,1,66,16,1,5);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (158,'2022-12-4','15:00',496,1,23,2,1,8);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (138,'2022-12-17','11:00',265,1,12,1,1,3);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (122,'2022-12-15','16:00',236,1,57,3,1,9);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (252,'2022-12-20','19:00',260,1,75,1,2,5);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (355,'2022-12-12','21:00',495,1,58,19,2,7);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (721,'2022-12-23','13:00',381,1,74,14,2,9);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (528,'2022-12-18','12:00',454,1,27,4,3,6);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (007,'2022-12-8','2:00',375,1,14,12,3,2);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (251,'2022-12-22','6:00',308,1,29,1,3,10);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (253,'2022-12-9','16:00',272,1,27,7,3,5);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (911,'2022-12-20','21:00',157,1,83,16,4,5);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (921,'2022-12-18','14:00',269,1,27,9,4,9);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (931,'2022-12-6','14:00',164,1,61,16,4,8);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (951,'2022-12-28','4:00',223,1,32,11,4,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (191,'2022-12-3','20:00',246,1,64,12,5,7);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (291,'2022-12-28','19:00',456,1,88,4,5,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (391,'2022-12-17','2:00',486,1,1,12,5,10);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (491,'2022-12-18','10:00',314,1,6,6,5,2);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (511,'2022-12-16','16:00',442,1,80,19,6,2);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (512,'2022-12-26','8:00',418,1,1,14,6,10);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (515,'2022-12-19','13:00',133,1,25,3,6,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (519,'2022-12-21','10:00',346,1,15,2,6,5);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (520,'2022-12-3','3:00',447,1,22,10,6,7);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (271,'2022-12-9','12:00',230,1,72,18,7,10);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (617,'2022-12-29','12:00',148,1,96,13,7,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (712,'2022-12-21','16:00',201,1,93,3,7,3);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (365,'2022-12-18','12:00',134,1,15,15,7,3);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (808,'2022-12-3','3:00',183,1,44,16,8,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (708,'2022-12-18','17:00',132,1,53,16,8,7);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (807,'2022-12-17','1:00',371,1,5,10,8,9);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (854,'2022-12-24','18:00',122,1,67,6,8,10);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (912,'2022-12-25','18:00',448,1,70,12,9,10);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (192,'2022-12-1','5:00',270,1,37,5,9,1);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (592,'2022-12-3','19:00',196,1,74,12,9,2);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (952,'2022-12-17','7:00',411,1,26,5,9,3);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (915,'2022-12-13','3:00',208,1,42,18,9,6);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (755,'2022-12-17','13:00',223,1,21,19,10,4);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (855,'2022-12-31','19:00',468,1,80,11,10,9);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (955,'2022-12-14','10:00',265,1,77,16,10,8);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (555,'2022-12-6','7:00',234,1,93,16,10,6);
+insert into Flight(fnumber,fdate,ftime,price,class,capacity,available,orig,dest) values (575,'2022-12-22','5:00',441,1,41,13,10,5);
+
+
+-----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+insert into customer(cname,email,address,`password`) values ('Karan','karan123@gmail.com','3211 Kedzie, Chicago','xyz123@');
+insert into customer(cname,email,address,`password`) values ('Ronaldo','ronaldo@gmail.com','5216 Michigan Ave','Rona@1234');
+
+
